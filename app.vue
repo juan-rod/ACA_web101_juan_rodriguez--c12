53d62ef00880c13905f07260aebbb2ca8c964789
jrod <jrod@Juans-MacBook-Pro.local>
2020-12-14 19:50:21 -0600

    init resume page

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="nav">
+    nav
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  methods: {
+    getUserID () {
+      return user.id
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
